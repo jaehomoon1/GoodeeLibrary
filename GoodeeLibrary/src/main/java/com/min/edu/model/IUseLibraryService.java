06408884ff82bedf37e9e1dfaea34d32f5765603
dus404 <dus404@naver.com>
2021-12-16 10:22:11 +0900

    작성자 : 강남호 내용 : 도서관 이용 VO 생성

---
+++ GoodeeLibrary/src/main/java/com/min/edu/model/IUseLibraryService.java
@@ -0,0 +1,12 @@
+package com.min.edu.model;
+
+import java.util.Map;
+
+public interface IUseLibraryService {
+
+	
+	
+	public int insertLoan(Map<String, Object> map);
+	public int insertBookDetail(Map<String, Object> map);
+	
+}
