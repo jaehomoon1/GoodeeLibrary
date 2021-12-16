9ae9942853ff183ed638e0b74c6afa0cd43ae942
dus404 <dus404@naver.com>
2021-12-16 17:46:53 +0900

    작성자 : 강남호 내용 : 파일위치 변경

--- GoodeeLibrary/src/main/java/com/min/edu/controller/BookMemberController.java
+++ GoodeeLibrary/src/main/java/com/min/edu/controller/BookMemberController.java
@@ -15,9 +15,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.support.SessionStatus;
 
-import com.min.edu.model.IBookMemberDao;
+import com.min.edu.model.IBookMemberService;
 import com.min.edu.vo.BookMemberVo;
 
 @Controller
@@ -26,7 +25,9 @@ public class BookMemberController {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
-	private IBookMemberDao dao;
+	private IBookMemberService service;
+	
+	
 	
 	@RequestMapping(value = "/login.do", method = RequestMethod.POST)
 	public String login(@RequestParam("id") String id, @RequestParam("pw") String pw,
@@ -34,30 +35,36 @@ public class BookMemberController {
 		BookMemberVo vo = new BookMemberVo();
 		vo.setId(id);
 		vo.setPassword(pw);
-		BookMemberVo result = dao.loginMember(vo);
+		BookMemberVo result = service.loginMember(vo);
 		if(result == null) {
 			resp.setContentType("text/html; charset=UTF-8");
 			PrintWriter out = resp.getWriter();
 			out.println("<script>alert('아이디나 비밀번호가 일치하지 않습니다.');</script>");
 			out.flush();
-			return "loginForm";
+			return "member/loginForm";
 		}
 		session.setAttribute("mvo", result);
 		logger.info("session에 들어있는 값 : {}", session.getAttribute("mvo"));
-		return "login";
+		return "member/bookMain";
 	}
 	
 	@GetMapping(value = "/loginForm.do")
 	public String loginForm() {
 		logger.info("loginForm으로 이동");
-		return "loginForm";
+		return "member/loginForm";
 	}
 	
 	@GetMapping(value = "/logout.do")
-	public String logout(HttpSession session, SessionStatus sessionStatus) {
-		sessionStatus.setComplete();
+	public String logout(HttpSession session) {
+		session.invalidate();
 		logger.info("세션 Cleanup");
-		return null;
+		return "member/bookMain";
+	}
+	
+	@GetMapping(value = "/signupForm.do")
+	public String signupForm() {
+		logger.info("signupForm 이동");
+		return "member/signupForm";
 	}
 	
 }
